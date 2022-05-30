b0d82d58cbdd59ba2e4a348f2e5bee9f07fd94d5
Vikramaditya Karanwal <adityavkrm00@gmail.com>
2022-05-30 19:06:06 +0530

    test

--- terraform-manifests/05-log-analytics-workspace.tf
+++ terraform-manifests/05-log-analytics-workspace.tf
@@ -1,5 +1,10 @@
 # Create Log Analytics Workspace
-# Log Analytics workspace will be created per environment.
+/* 
+Log Analytics workspace will be created per environment.
+Example:
+stage-logs-some-random-petname
+prod-logs-some-random-petname
+*/
 resource "azurerm_log_analytics_workspace" "insights" {
   name                = "${var.environment}-logs-${random_pet.aksrandom.id}"
   location            = azurerm_resource_group.aks_rg.location
