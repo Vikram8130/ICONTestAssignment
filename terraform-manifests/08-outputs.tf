a262aa80174828733827bc367e04c063dbc492fd
Vikramaditya Karanwal <adityavkrm00@gmail.com>
2022-05-30 19:17:38 +0530

    test

--- terraform-manifests/08-outputs.tf
+++ terraform-manifests/08-outputs.tf
@@ -16,6 +16,13 @@ output "resource_group_name" {
   value = azurerm_resource_group.aks_rg.name
 }
 
+/* AKS Cluster
+AKS Versions
+AKS Latest Version
+AKS Cluster ID
+AKS Cluster Name
+AKS Cluster Kubernetes Version
+*/
 # Azure AKS Versions Datasource
 output "versions" {
   value = data.azurerm_kubernetes_service_versions.current.versions
@@ -24,26 +31,23 @@ output "versions" {
 output "latest_version" {
   value = data.azurerm_kubernetes_service_versions.current.latest_version
 }
-
-# Azure AD Group Object Id
-output "azure_ad_group_id" {
-  value = azuread_group.aks_administrators.id
-}
-output "azure_ad_group_objectid" {
-  value = azuread_group.aks_administrators.object_id
-}
-
-
 # Azure AKS Outputs
-
 output "aks_cluster_id" {
   value = azurerm_kubernetes_cluster.aks_cluster.id
 }
-
 output "aks_cluster_name" {
   value = azurerm_kubernetes_cluster.aks_cluster.name
 }
 
 output "aks_cluster_kubernetes_version" {
   value = azurerm_kubernetes_cluster.aks_cluster.kubernetes_version
+}
+
+/* AD Group: Group ID , Object ID */
+# Azure AD Group Object Id
+output "azure_ad_group_id" {
+  value = azuread_group.aks_administrators.id
+}
+output "azure_ad_group_objectid" {
+  value = azuread_group.aks_administrators.object_id
 }
